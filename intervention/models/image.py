59c9fd3134416ff03f0672ffcc16e76ff07f922e
Thomas Churchman <thomas@kepow.org>
2020-08-27 12:41:55 +0300

    Add image-agent network module

--- intervention/models/image.py
+++ intervention/models/image.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from .spatial_softargmax import SpatialSoftargmax
+
 class TaillessResnet34(nn.Module):
     """
     Resnet from torchvision without the average pooling, flattening, and
@@ -26,3 +28,57 @@ class TaillessResnet34(nn.Module):
         image = self._resnet.layer4(image)
 
         return image
+
+
+class Image(nn.Module):
+    OUTPUTS: int = 4
+    HEATMAP_WIDTH: int = 96
+    HEATMAP_HEIGHT: int = 40
+    COORDINATE_STEPS: int = 5
+
+    def __init__(self):
+        super().__init__()
+
+        self.resnet = TaillessResnet34()
+
+        self.higher = nn.Sequential(
+            nn.BatchNorm2d(512 + 1),
+            nn.ConvTranspose2d(512 + 1, 256, 3, 2, 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(256),
+            nn.ConvTranspose2d(256, 128, 3, 2, 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 64, 3, 2, 1, 1),
+            nn.ReLU(True),
+        )
+
+        self.location_prediction = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.BatchNorm2d(64),
+                    nn.Conv2d(64, Image.COORDINATE_STEPS, 1, 1, 0),
+                    SpatialSoftargmax(
+                        Image.HEATMAP_WIDTH,
+                        Image.HEATMAP_HEIGHT,
+                        Image.COORDINATE_STEPS,
+                    ),
+                )
+                for _ in range(Image.OUTPUTS)
+            ]
+        )
+
+    def forward(self, image, velocity):
+        resnet_out = self.resnet(image)
+
+        velocity = velocity.repeat((1, 1, 5, 12))
+        higher_in = torch.cat((resnet_out, velocity), dim=1)
+
+        higher_out = self.higher(higher_in)
+
+        location_predictions = [
+            location_prediction(higher_out)
+            for location_prediction in self.location_prediction
+        ]
+
+        return location_predictions
