2d6758c88b58dff937dc91ab17171be3e87c556a
Thomas Churchman <thomas@kepow.org>
2020-08-27 12:36:46 +0300

    Add spatial softargmax

---
+++ intervention/models/spatial_softargmax.py
@@ -0,0 +1,51 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+import numpy as np
+
+
+class SpatialSoftargmax(nn.Module):
+    """
+    From: https://gist.github.com/jeasinema/1cba9b40451236ba2cfb507687e08834
+    """
+
+    def __init__(self, height, width, channel, temperature=None, data_format="NCHW"):
+        super().__init__()
+        self.data_format = data_format
+        self.height = height
+        self.width = width
+        self.channel = channel
+
+        if temperature:
+            self.temperature = Parameter(torch.ones(1) * temperature)
+        else:
+            self.temperature = 1.0
+
+        pos_x, pos_y = np.meshgrid(
+            np.linspace(-1.0, 1.0, self.height), np.linspace(-1.0, 1.0, self.width)
+        )
+        pos_x = torch.from_numpy(pos_x.reshape(self.height * self.width)).float()
+        pos_y = torch.from_numpy(pos_y.reshape(self.height * self.width)).float()
+        self.register_buffer("pos_x", pos_x)
+        self.register_buffer("pos_y", pos_y)
+
+    def forward(self, feature):
+        # Output:
+        #   (N, C*2) x_0 y_0 ...
+        if self.data_format == "NHWC":
+            feature = (
+                feature.transpose(1, 3)
+                .tranpose(2, 3)
+                .view(-1, self.height * self.width)
+            )
+        else:
+            feature = feature.view(-1, self.height * self.width)
+
+        softmax_attention = F.softmax(feature / self.temperature, dim=-1)
+        expected_x = torch.sum(self.pos_x * softmax_attention, dim=1, keepdim=True)
+        expected_y = torch.sum(self.pos_y * softmax_attention, dim=1, keepdim=True)
+        expected_xy = torch.cat([expected_x, expected_y], 1)
+        feature_keypoints = expected_xy.view(-1, self.channel * 2)
+
+        return feature_keypoints
