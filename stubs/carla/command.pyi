a7b2e88b0c5b809318d0d65784de27331618f583
Thomas Churchman <thomas@kepow.org>
2020-11-02 14:55:12 +0200

    Add `command.Response` type

--- stubs/carla/command.pyi
+++ stubs/carla/command.pyi
@@ -7,6 +7,14 @@ class Command:
     ...
 
 
+class Response:
+    actor_id: int
+    error: str
+
+    def has_error(self) -> bool:
+        ...
+
+
 class DestroyActor(Command):
     def __init__(self, actor: Actor):
         ...
