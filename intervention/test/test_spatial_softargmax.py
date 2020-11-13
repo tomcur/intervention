d60a1dbcbd6ec17a12665ec21a056ad1fef7abbf
Thomas Churchman <thomas@kepow.org>
2020-11-13 14:28:37 +0200

    Add spatial softargmax gradient test

--- intervention/test/test_spatial_softargmax.py
+++ intervention/test/test_spatial_softargmax.py
@@ -58,3 +58,76 @@ class Test(unittest.TestCase):
 
             self.assertTrue(abs(0.5 - heatmap[0, 0, y1, x1].item()) < Test.EPSILON)
             self.assertTrue(abs(0.5 - heatmap[0, 0, y2, x2].item()) < Test.EPSILON)
+
+
+class TestGradients(unittest.TestCase):
+    def test_argmax_gradient_direction(self):
+        tensor = torch.tensor(
+            [
+                [
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0],
+                        [1.0, 1.0, 1.0, 0.0, 0.0],
+                        [1.0, 1.0, 1.0, 0.0, 0.0],
+                        [1.0, 1.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0],
+                    ]
+                ]
+            ],
+            requires_grad=True,
+        )
+        ssam = SpatialSoftargmax(7, 5, 1)
+        coords, _ = ssam(tensor)
+
+        # Set target expected value to positive-X.
+        diff = torch.abs(coords - torch.tensor([1.0, 0.0]))
+        loss = torch.abs(diff.pow(2)).mean()
+        loss.backward()
+
+        # We expect the negative gradient to be to positive-X.
+        expected_positive_loss = torch.tensor(
+            [
+                [
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                    [True, True, False, False, False],
+                ]
+            ]
+        )
+
+        self.assertTrue(torch.all(expected_positive_loss.eq(tensor.grad > 0)))
+
+        # Make a small step in direction of negative loss.
+        with torch.no_grad():
+            tensor -= 0.1 * tensor.grad
+            grad = tensor.grad.detach().clone()
+            tensor.grad.zero_()
+
+        new_coords, _ = ssam(tensor)
+        old_argmax_x, old_argmax_y = coords[0, 0].tolist()
+        new_argmax_x, new_argmax_y = new_coords[0, 0].tolist()
+
+        # We expect the argmax X to have moved in direction of positive-X...
+        self.assertGreater(new_argmax_x, old_argmax_x)
+
+        # ... but for the argmax Y to stay the same.
+        self.assertAlmostEqual(old_argmax_y, new_argmax_y, places=6)
+
+        # If instead we make a step in direction of positive loss...
+        with torch.no_grad():
+            tensor += 0.2 * grad
+
+        new_coords, _ = ssam(tensor)
+        new_argmax_x, new_argmax_y = new_coords[0, 0].tolist()
+
+        # We expect the argmax X to have moved to negative-X...
+        self.assertLess(new_argmax_x, old_argmax_x)
+
+        # ... and for the argmax Y to still stay the same.
+        self.assertAlmostEqual(old_argmax_y, new_argmax_y, places=6)
