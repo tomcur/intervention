52df48d2ff0cba8080b4df1aa126e19e044bc28f
Thomas Churchman <thomas@kepow.org>
2021-01-16 00:05:47 +0100

    Improve setup.py, use requirements.txt for pinning

--- setup.py
+++ setup.py
@@ -15,6 +15,23 @@ setup(
     author="Thomas Churchman",
     author_email="thomas@kepow.org",
     packages=["intervention",],
-    install_requires=[],
+    install_requires=[
+        # Generic:
+        "typing-extensions~=3.7",
+        "loguru~=0.4",
+        "click~=7.0",
+        "dataclass-csv~=1.1",
+        # Image processing / models:
+        "tensorboard~=2.4",
+        "torch~=1.5",
+        "torchvision~=0.7",
+        "opencv-python~=4",
+        "pygame~=1.9",
+        "pillow~=8.1",
+        # Computation:
+        "numpy~=1.19",
+        "numpy-stubs",
+        "networkx~=2.5",
+    ],
     scripts=["scripts/intervention-learning"],
 )
