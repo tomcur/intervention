b2b5de50413e55c38c57ac3a9b12e06135be99da
Thomas Churchman <thomas@kepow.org>
2021-06-09 12:27:13 +0200

    Limit buildable systems in Nix flake

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     flake = false;
   };
   outputs = { self, nixpkgs, flake-utils, libjpeg, libproj, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in
       rec {
