79ab7c93cd9e484dbc2f3e3f9b02b6eb67378adb
Thomas Churchman <thomas@kepow.org>
2021-08-12 14:58:45 +0100

    Normalize in-place

--- intervention/train/dataset.py
+++ intervention/train/dataset.py
@@ -187,7 +187,7 @@ class _Dataset(torch.utils.data.Dataset):
             [
                 torchvision.transforms.ToTensor(),
                 torchvision.transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], inplace=True
                 ),
             ]
         )
