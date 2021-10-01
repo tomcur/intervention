f35ba6f545c51331d7c7180f3d5c18c1f8df2deb
Thomas Churchman <thomas@kepow.org>
2021-10-02 01:27:07 +0200

    Clean up

--- intervention/coordinates.py
+++ intervention/coordinates.py
@@ -8,56 +8,15 @@ CAMERA_FORWARD_OFFSET = 2.0
 
 PIXELS_PER_METER = 5
 PIXEL_OFFSET = 10  # CAMERA_FORWARD_OFFSET * PIXELS_PER_METER
-OFFSET = (-80.0, 160.0)
+
+# For the world map as used by the teacher (and visualization)
+BIRDVIEW_OFFSET = (-80.0, 160.0)
 BIRDVIEW_IMAGE_SIZE = 320
 BIRDVIEW_CROP_SIZE = 192
-ANGLE_JITTER = 15
 
 
 def carla_to_opencv(carla_xyz: np.ndarray) -> np.ndarray:
     return np.array([carla_xyz[..., 0], -carla_xyz[..., 2], carla_xyz[..., 1]]).T
-
-
-def world_coordinate_to_birdview_coordinate(
-    location_x: float,
-    location_y: float,
-    current_location_x: float,
-    current_location_y: float,
-    current_forward_x: float,
-    current_forward_y: float,
-):
-    """
-    Get the birdview coordinate of a world location relative to a current location and
-    orientation.
-
-    Based on
-    https://github.com/dianchen96/LearningByCheating/blob/031308a77a8ca7e9325ae909ebe04a34105b5d81/bird_view/utils/datasets/image_lmdb.py
-
-    :param location_x: The x-component of a world location.
-    :param location_y: The y-component of a world location.
-    :param current_location_x: The x-component of the current world location.
-    :param current_location_y: The y-component of the current world location.
-    :param current_forward_x: The x-component of the vector pointing forwards according
-    to the current orientation.
-    :param current_forward_y: The y-component of the vector pointing forwards according
-    to the current orientation.
-    """
-    birdview_dx = (location_x - current_location_x) * PIXELS_PER_METER
-    birdview_dy = (location_y - current_location_y) * PIXELS_PER_METER
-
-    birdview_x = -birdview_dx * current_forward_y + birdview_dy * current_forward_x
-    birdview_y = BIRDVIEW_IMAGE_SIZE - (
-        birdview_dx * current_forward_x + birdview_dy * current_forward_y
-    )
-
-    birdview_x += OFFSET[1]
-    birdview_y += OFFSET[0]
-
-    birdview_x -= (BIRDVIEW_IMAGE_SIZE - BIRDVIEW_CROP_SIZE) // 2
-    birdview_y = BIRDVIEW_CROP_SIZE - (BIRDVIEW_IMAGE_SIZE - birdview_y) + 70
-
-    birdview_y += PIXEL_OFFSET
-    return birdview_x, birdview_y
 
 
 def world_coordinate_to_ego_coordinate(
@@ -233,3 +192,45 @@ def image_coordinate_to_ego_coordinate(
     world_x = world_y * x
 
     return world_x, world_y - forward_offset + CAMERA_FORWARD_OFFSET
+
+
+def world_coordinate_to_birdview_coordinate(
+    location_x: float,
+    location_y: float,
+    current_location_x: float,
+    current_location_y: float,
+    current_forward_x: float,
+    current_forward_y: float,
+):
+    """
+    Get the birdview coordinate of a world location relative to a current location and
+    orientation.
+
+    Based on
+    https://github.com/dianchen96/LearningByCheating/blob/031308a77a8ca7e9325ae909ebe04a34105b5d81/bird_view/utils/datasets/image_lmdb.py
+
+    :param location_x: The x-component of a world location.
+    :param location_y: The y-component of a world location.
+    :param current_location_x: The x-component of the current world location.
+    :param current_location_y: The y-component of the current world location.
+    :param current_forward_x: The x-component of the vector pointing forwards according
+    to the current orientation.
+    :param current_forward_y: The y-component of the vector pointing forwards according
+    to the current orientation.
+    """
+    birdview_dx = (location_x - current_location_x) * PIXELS_PER_METER
+    birdview_dy = (location_y - current_location_y) * PIXELS_PER_METER
+
+    birdview_x = -birdview_dx * current_forward_y + birdview_dy * current_forward_x
+    birdview_y = BIRDVIEW_IMAGE_SIZE - (
+        birdview_dx * current_forward_x + birdview_dy * current_forward_y
+    )
+
+    birdview_x += BIRDVIEW_OFFSET[1]
+    birdview_y += BIRDVIEW_OFFSET[0]
+
+    birdview_x -= (BIRDVIEW_IMAGE_SIZE - BIRDVIEW_CROP_SIZE) // 2
+    birdview_y = BIRDVIEW_CROP_SIZE - (BIRDVIEW_IMAGE_SIZE - birdview_y) + 70
+
+    birdview_y += PIXEL_OFFSET
+    return birdview_x, birdview_y
